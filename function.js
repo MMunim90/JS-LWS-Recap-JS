d68b48abeaf7ce309be99e76b251d90b4eb8d11b
MMunim <mmunim9.9.01@gmail.com>
2025-10-01 15:02:27 +0600

    working on function

--- function.js
+++ function.js
@@ -1,8 +1,30 @@
 // regular function
 
-function hello(){
-    console.log("Hello World");
-}
+// function hello(){
+//     console.log("Hello World");
+//      return undefined
+// }
 
-let msg = hello();
-console.log(msg);
+// let msg = hello();
+// console.log(msg);
+
+
+//function expression
+// const hello = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(hello)
+
+//named function expression
+// const hello = function hello(){
+//     console.log("Hello world");
+// }
+
+// hello();
+
+
+// arrow function
+const hello = () => {
+    console.log("Hello world");
+}
