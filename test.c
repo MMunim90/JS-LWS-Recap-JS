bffec23e3ef3791d59c1fcf6182dce02372d1f8b
MMunim <mmunim9.9.01@gmail.com>
2025-10-03 18:19:01 +0600

    changes in c program

--- test.c
+++ test.c
@@ -4,11 +4,20 @@
 //     return 0;
 // }
 
-#include<stdio.h>
+/*#include<stdio.h>
 int main(){
     int a;
     scanf("%d", &a);
     printf("a = %d", a);
 
+    return 0;
+}*/
+
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d", &a);
+    printf("The number is %d\n", a);
+    printf("Hello World");
     return 0;
 }
