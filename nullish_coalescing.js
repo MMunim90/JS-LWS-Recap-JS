cb99ff878669e719ac043600782cf77a7b877d2d
MMunim <mmunim9.9.01@gmail.com>
2025-10-04 14:42:34 +0600

    explore nullish coalescing operator

---
+++ nullish_coalescing.js
@@ -0,0 +1,9 @@
+// Nullish Coalescing
+
+// Nullish means Null or Undefined
+
+// let lang = null;
+let lang = false;
+console.log(lang ?? "JavaScript");
+console.log(lang || "JavaScript");
+console.log(lang && "JavaScript");
